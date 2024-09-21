7f956c1e41a1132f4bd05a8f776bbf1a26e0e2df
Adam Dilger <adamdilger1@gmail.com>
2024-09-21 12:41:00 +1000

    added node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,14 @@ import tailwind from "@astrojs/tailwind";
 
 import solidJs from "@astrojs/solid-js";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [db(), tailwind(), solidJs()],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
